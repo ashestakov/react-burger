f887edbec4c474360ba4c1d5cc7c5e2c6bc1920a
ashestackov <andr@funzavod.ru>
2022-06-11 17:05:53 +0400

    Process fetch errors in reducers

--- src/services/reducers/ingredients.js
+++ src/services/reducers/ingredients.js
@@ -1,4 +1,4 @@
-import {INGREDIENTS_SUCCESS} from "../actions/ingredients";
+import {INGREDIENTS_SUCCESS, INGREDIENTS_ERROR} from "../actions/ingredients";
 
 const INITIAL_STATE = [];
 
@@ -6,6 +6,8 @@ export default function ingredients(state = INITIAL_STATE, action) {
   switch (action.type) {
     case INGREDIENTS_SUCCESS:
       return action.ingredients
+    case INGREDIENTS_ERROR:
+      return INITIAL_STATE
     default:
       return state;
   }
--- src/services/reducers/placed-order.js
+++ src/services/reducers/placed-order.js
@@ -1,9 +1,11 @@
-import {PLACED_ORDER_SUCCESS, PLACED_ORDER_RESET} from "../actions/placed-order";
+import {PLACED_ORDER_SUCCESS, PLACED_ORDER_RESET, PLACED_ORDER_ERROR} from "../actions/placed-order";
 
 export default function placedOrder(state = null, action) {
   switch (action.type) {
     case PLACED_ORDER_SUCCESS:
       return action.order;
+    case PLACED_ORDER_ERROR:
+      return null;
     case PLACED_ORDER_RESET:
       return null;
     default:
