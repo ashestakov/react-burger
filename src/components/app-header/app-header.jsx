717636dd18b7962988277067842fe351e1683a0f
ashestackov <andr@funzavod.ru>
2022-06-26 15:31:41 +0400

    Make a link to the main page

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,32 @@
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './app-header.module.css'
+import NavBarLink from "../nav-bar-link/nav-bar-link";
+import {Link} from "react-router-dom";
+
+export default function AppHeader() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navBar}>
+        <div className={styles.leftHalf}>
+          <NavBarLink to={'/'} exact={true}>
+            <BurgerIcon type="primary"/>
+            Конструктор
+          </NavBarLink>
+          <NavBarLink to={'#'} exact={true}>
+            <ListIcon type="primary"/>
+            Лента заказов
+          </NavBarLink>
+        </div>
+        <Link to={'/'}>
+          <Logo/>
+        </Link>
+        <div className={styles.rightHalf}>
+          <NavBarLink to={'/profile'} exact={false}>
+            <ProfileIcon type="primary"/>
+            Личный кабинет
+          </NavBarLink>
+        </div>
+      </nav>
+    </header>
+  );
+}
