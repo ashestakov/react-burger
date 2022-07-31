66510054b972ab9cc8f1259d873bc6fc2d53109d
andrey <andr@funzavod.ru>
2022-07-31 16:54:55 +0500

    Replace action type literals with constants

--- src/middleware/socketMiddleware.ts
+++ src/middleware/socketMiddleware.ts
@@ -2,6 +2,13 @@ import type { Middleware, MiddlewareAPI } from 'redux';
 import {AppDispatch} from "../hooks";
 import {RootState} from "../services/reducers/root-reducer";
 import {WSAction} from "../services/reducers/websocket";
+import {
+  WS_CONNECTION_CLOSED,
+  WS_CONNECTION_ERROR,
+  WS_CONNECTION_START,
+  WS_CONNECTION_SUCCESS,
+  WS_GET_MESSAGE, WS_SEND_MESSAGE
+} from "../services/actions/websocket";
 
 export const socketMiddleware = (): Middleware => {
   return ((store: MiddlewareAPI<AppDispatch, RootState>) => {
@@ -11,7 +18,7 @@ export const socketMiddleware = (): Middleware => {
       const { dispatch, getState } = store;
       const { type, payload } = action;
 
-      if (type === 'WS_CONNECTION_START') {
+      if (type === WS_CONNECTION_START) {
         if (socket) {
           socket.close();
         }
@@ -20,22 +27,22 @@ export const socketMiddleware = (): Middleware => {
       if (socket) {
 
         socket.onopen = event => {
-          dispatch({ type: 'WS_CONNECTION_SUCCESS', payload: event });
+          dispatch({ type: WS_CONNECTION_SUCCESS, payload: event });
         };
 
         socket.onerror = event => {
-          dispatch({ type: 'WS_CONNECTION_ERROR', payload: event });
+          dispatch({ type: WS_CONNECTION_ERROR, payload: event });
         };
 
         socket.onmessage = event => {
           const { data } = event;
-          dispatch({ type: 'WS_GET_MESSAGE', payload: data });
+          dispatch({ type: WS_GET_MESSAGE, payload: data });
         };
         socket.onclose = event => {
-          dispatch({ type: 'WS_CONNECTION_CLOSED', payload: event });
+          dispatch({ type: WS_CONNECTION_CLOSED, payload: event });
         };
 
-        if (type === 'WS_SEND_MESSAGE') {
+        if (type === WS_SEND_MESSAGE) {
           const message = payload;
           socket.send(JSON.stringify(message));
         }
